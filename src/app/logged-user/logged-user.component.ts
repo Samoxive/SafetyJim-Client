e3cb8c552be37ce662a20ce0d112bc1de2639b6b
Şamil NART <samil.nart@gmail.com>
2017-12-20 15:03:52 +0300

    Pull logged in user's data from jim

--- src/app/logged-user/logged-user.component.ts
+++ src/app/logged-user/logged-user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { User } from '../entities/user';
 
 @Component({
     selector: 'app-logged-user',
@@ -8,17 +11,34 @@ import { LoginService } from '../login.service';
 })
 export class LoggedUserComponent implements OnInit {
     public isLogged = false;
+    public username = '';
+    public avatarSrc = '';
 
-    constructor(private loginService: LoginService) { }
+    constructor(private loginService: LoginService,
+                private http: HttpClient) { }
 
     ngOnInit() {
         this.isLogged = this.loginService.isLoggedIn();
+        this.fetchSelf();
         this.loginService.subscription()
-            .subscribe((loggedStatus) => this.isLogged = loggedStatus);
+            .subscribe((loggedStatus) => {
+                this.isLogged = loggedStatus;
+                this.fetchSelf();
+            });
     }
 
     logOut() {
         this.loginService.logout();
     }
 
+    async fetchSelf() {
+        if (this.isLogged) {
+            const user = await this.http.get(`${environment.apiUrl}/self`, { headers: { 'token': this.loginService.getToken() } })
+                                 .toPromise()
+                                 .then((u) => u as User);
+
+            this.username = `${user.username}#${user.discriminator}`;
+            this.avatarSrc = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+        }
+    }
 }
