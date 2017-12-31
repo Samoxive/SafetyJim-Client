9b9c458f570d90b45547fee50a5d17f13152a9f4
Şamil NART <samil.nart@gmail.com>
2017-12-31 22:22:09 +0300

    Move selected guild to the service, add reactive methods

--- src/app/guild.service.ts
+++ src/app/guild.service.ts
@@ -5,26 +5,36 @@ import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class GuildService {
     private guildsObservable: Observable<any>;
-    private guilds: Guild[] = [];
+    public guilds: Guild[] = [];
+    public selectedGuild: Guild = null;
+    public selectedGuildSubject = new Subject<Guild>();
+    private fetchedGuilds = false;
     private fetchingGuilds = false;
 
     constructor(private http: HttpClient,
                 private loginService: LoginService) {}
 
     public fetchGuilds(): Promise<Guild[]> {
+        if (this.fetchedGuilds) {
+            return Promise.resolve(this.guilds);
+        }
+
         if (this.loginService.isLoggedIn()) {
             const token = this.loginService.getToken();
-            if (this.guildsObservable == null) {
+            if (!this.fetchingGuilds) {
                 this.guildsObservable = this.http.get(environment.apiUrl + '/guilds', { headers: new HttpHeaders().set('token', token) });
             }
 
             return new Promise((resolve, reject) => {
                 this.guildsObservable.subscribe((data) => {
                     this.guilds = data as Guild[];
+                    this.fetchingGuilds = false;
+                    this.fetchedGuilds = true;
                     resolve(this.guilds);
                 });
             });
@@ -33,11 +43,23 @@ export class GuildService {
         }
     }
 
-    public async getGuild(guildId: string): Promise<Guild> {
-        if (this.loginService.isLoggedIn() && this.guilds.length === 0) {
+    // returns true == success
+    public async selectGuild(guildId: string): Promise<boolean> {
+        if (!this.fetchedGuilds) {
             await this.fetchGuilds();
         }
-        return this.guilds.filter((guild) => guild.id === guildId)[0];
+
+        const g = this.guilds.filter((guild) => guildId === guild.id)[0]
+        if (g == null) {
+            return false;
+        } else {
+            this.selectedGuild = g;
+            this.selectedGuildSubject.next(g);
+            return true;
+        }
     }
 
+    public selectedGuildObservable(): Observable<Guild> {
+        return this.selectedGuildSubject.asObservable();
+    }
 }
