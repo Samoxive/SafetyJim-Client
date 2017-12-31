59a84e196e86c38513b6bda5361f2063ae79cd71
Şamil NART <samil.nart@gmail.com>
2017-12-31 22:22:43 +0300

    Add guild related components

--- src/app/guild-home/guild-home.component.ts
+++ src/app/guild-home/guild-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GuildService } from '../guild.service';
+import { ParamMap } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Guild } from '../entities/guild';
 
 @Component({
     selector: 'app-guild-home',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./guild-home.component.css']
 })
 export class GuildHomeComponent implements OnInit {
+    public selectedGuild: Guild;
+    public isGuildSelected = false;
 
-    constructor() { }
+    constructor(public guildService: GuildService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) {
+        this.changeSelectedGuild(guildService.selectedGuild);
+        this.guildService.selectedGuildObservable().subscribe((guild) => this.changeSelectedGuild(guild));
+    }
 
-    ngOnInit() {
+    async ngOnInit() {
+        this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+            const guildId = params.get('id');
+            if (guildId == null) {
+                this.router.navigate(['']);
+            } else {
+                this.guildService.selectGuild(guildId).then((success) => {
+                    if (!success) {
+                        this.router.navigate(['']);
+                    } else {
+                        this.changeSelectedGuild(this.guildService.selectedGuild);
+                    }
+                });
+            }
+        });
+    }
+
+    private changeSelectedGuild(guild: Guild) {
+        this.selectedGuild = guild;
+        this.isGuildSelected = guild == null ? false : true;
     }
 
 }
