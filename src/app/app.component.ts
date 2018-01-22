e9f4d08f314e466d5f6d71aa02ef59feebc6c04b
Şamil NART <samil.nart@gmail.com>
2018-01-22 17:27:48 +0300

    Add sidenav button click listener and move some code to ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { CookieService } from 'ngx-cookie';
 import { LoginService } from './login.service';
 import { Guild } from './entities/guild';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Router } from '@angular/router';
 import { GuildService } from './guild.service';
+import { MatSidenav } from '@angular/material';
+import { ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -14,10 +15,12 @@ import { GuildService } from './guild.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
+    @ViewChild('sidenav') private sidenav: MatSidenav;
     public mobileQuery: MediaQueryList;
     private mobileQueryListener: () => void;
     public title: String;
     public isLogged = false;
+    public selectedGuild = null;
     public isGuildSelected = false;
 
     constructor(private http: HttpClient,
@@ -29,11 +32,14 @@ export class AppComponent implements OnInit, OnDestroy {
         this.mobileQuery = media.matchMedia('(max-width: 600px');
         this.mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this.mobileQueryListener);
-        this.isGuildSelected = this.guildService.selectedGuild == null ? false : true;
-        this.guildService.selectedGuildObservable().subscribe((guild) => guild == null ? false : true);
     }
 
     public ngOnInit() {
+        this.selectedGuild = this.guildService.selectedGuild;
+        this.guildService.selectedGuildObservable().subscribe((guild) => {
+            this.selectedGuild = guild;
+            this.isGuildSelected = !!guild;
+        });
         this.isLogged = this.loginService.isLoggedIn();
         this.loginService.subscription()
             .subscribe((loggedStatus) => {
@@ -50,6 +56,16 @@ export class AppComponent implements OnInit, OnDestroy {
 
     public onGuildClick(guild: Guild) {
         this.router.navigate([`guild/${guild.id}`]);
+    }
+
+    public onSideNavClick(guild: Guild, item: string) {
+        if (item === 'stats') {
+            this.router.navigate([`guild/${guild.id}/statistics`]);
+        } else {
+            return;
+        }
+
+        this.sidenav.toggle();
     }
 
     private async populateGuilds() {
