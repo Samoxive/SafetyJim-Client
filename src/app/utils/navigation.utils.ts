58951f5a72da6b2c6d9a7445904c140d623caa2c
Şamil NART <samil.nart@gmail.com>
2018-01-26 21:56:19 +0300

    Add utility to allow components to require logged in user

--- src/app/utils/navigation.utils.ts
+++ src/app/utils/navigation.utils.ts
@@ -2,6 +2,9 @@ import { Router, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Guild } from '../entities/guild';
 import { GuildService } from '../guild.service';
+import { LoginService } from '../login.service';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { Component } from '@angular/core';
 
 export async function selectGuildOrRouteIndex(router: Router, paramMap: Observable<ParamMap>, guildService: GuildService):
                                                                                                                 Promise<boolean> {
@@ -20,4 +23,34 @@ export async function selectGuildOrRouteIndex(router: Router, paramMap: Observab
             }
         });
     });
+}
+
+export function userNeedsLogin(loginService: LoginService, dialog: MatDialog): boolean {
+    if (loginService.isLoggedIn()) {
+        return false;
+    } else {
+        dialog.open(LoginDialogComponent, {
+            closeOnNavigation: false,
+            disableClose: true,
+        });
+        return true;
+    }
+}
+
+@Component({
+    selector: 'app-login-dialog',
+    templateUrl: 'login-dialog.html'
+})
+export class LoginDialogComponent {
+    constructor (private dialog: MatDialogRef<LoginDialogComponent>, private router: Router) {}
+
+    navigateHome() {
+        this.router.navigateByUrl('/');
+        this.dialog.close();
+    }
+
+    login() {
+        this.router.navigateByUrl(`/login?redirectRoute=${encodeURI(btoa(location.pathname + location.search))}`);
+        this.dialog.close();
+    }
 }
