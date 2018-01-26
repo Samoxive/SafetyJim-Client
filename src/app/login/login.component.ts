324988f264022336fae058a29f12ed60d62e539b
Şamil NART <samil.nart@gmail.com>
2018-01-26 21:56:49 +0300

    If user opened a component and it required them to be logged in, let them go back to the component if they log in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,23 +21,34 @@ export class LoginComponent implements OnInit {
             return;
         }
 
-        const url = location.toString();
-        if (!url.includes('code')) {
-            location.replace('https://discordapp.com/api/oauth2/authorize?' +
+        const url = new URL(location.toString());
+        if (!url.searchParams.has('code')) {
+            let discordLoginUrl = 'https://discordapp.com/api/oauth2/authorize?' +
                 'client_id=' + environment.clientId +
                 '&redirect_uri=' + environment.redirectUri +
                 '&response_type=code' +
-                '&scope=guilds%20identify');
+                '&scope=guilds%20identify';
+
+            if (url.searchParams.has('redirectRoute')) {
+                discordLoginUrl += '&state=' + url.searchParams.get('redirectRoute');
+            }
+
+            location.replace(discordLoginUrl);
         } else {
-            const code = (new URL(url)).searchParams.get('code');
+            const code = url.searchParams.get('code');
             const tokenPromise = this.http.get(environment.apiUrl + '/login?code=' + code)
                                           .toPromise();
-            this.router.navigateByUrl('/');
+
             try {
                 const token = (await tokenPromise).toString();
                 this.loginService.login(token);
-            } catch (e) {
-
+            } finally {
+                if (url.searchParams.has('state')) {
+                    const redirect = atob(decodeURI(url.searchParams.get('state')));
+                    this.router.navigateByUrl(redirect);
+                } else {
+                    this.router.navigateByUrl('/');
+                }
             }
         }
     }
