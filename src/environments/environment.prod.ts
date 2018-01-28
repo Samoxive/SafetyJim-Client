e7fe3e81f12537b5ad53158d236f437f61ac1b52
Şamil NART <samil.nart@gmail.com>
2018-01-28 18:39:05 +0300

    Update production values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    redirectUri: 'http://localhost:4200/login',
-    clientId: '322714040344510464',
-    apiUrl: 'http://localhost:8080'
+    redirectUri: 'http://safetyjim.xyz',
+    clientId: '313749262687141888',
+    apiUrl: 'http://api.safetyjim.xyz'
 };
