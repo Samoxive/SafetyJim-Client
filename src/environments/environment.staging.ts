250db85a3c614bf4363c22ea57b216b04f78a57a
Şamil NART <samil.nart@gmail.com>
2018-01-28 19:24:59 +0300

    Replace incorrect environment values

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    redirectUri: 'http://safetyjim.xyz',
+    redirectUri: 'http://staging.safetyjim.xyz/login',
     clientId: '322714040344510464',
-    apiUrl: 'http://api.safetyjim.xyz'
+    apiUrl: 'http://api.staging.safetyjim.xyz'
 };
