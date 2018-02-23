47c6a5df160ecf13646b05b9d7e2a0a4fe8f4dad
Şamil NART <samil.nart@gmail.com>
2018-02-23 17:22:45 +0300

    Rework how commands are displayed

--- src/app/commands/commands.component.ts
+++ src/app/commands/commands.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Command } from './command';
 import { commands } from './commands';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
     selector: 'app-commands',
@@ -9,10 +10,27 @@ import { commands } from './commands';
 })
 export class CommandsComponent implements OnInit {
     public commands: Command[] = commands;
-    constructor() { }
+    constructor(private dialog: MatDialog) { }
 
     ngOnInit() {
 
     }
 
+    onDetailClick(data: String[]) {
+        this.dialog.open(CommandDetailComponent, {data});
+    }
+
+}
+
+@Component({
+    selector: 'app-command-detail-component',
+    templateUrl: 'command-detail.component.html'
+})
+export class CommandDetailComponent {
+    constructor(@Inject(MAT_DIALOG_DATA) public data: String[],
+                public dialogRef: MatDialogRef<CommandDetailComponent>) {}
+
+    onClose() {
+        this.dialogRef.close();
+    }
 }
