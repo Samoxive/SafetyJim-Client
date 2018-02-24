0bd1fd277308ffbbebc9bdb36273a75a9c911e84
Şamil NART <samil.nart@gmail.com>
2018-02-24 22:18:46 +0300

    Remove GuildHomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,6 @@ import { LoginService } from './login.service';
 import { GuildService } from './guild.service';
 import { LoggedUserComponent } from './logged-user/logged-user.component';
 import { CommandsComponent, CommandDetailComponent } from './commands/commands.component';
-import { GuildHomeComponent } from './guild-home/guild-home.component';
 import { GuildSettingsComponent } from './guild-settings/guild-settings.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -52,7 +51,6 @@ const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'commands', component: CommandsComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'guild/:id', component: GuildHomeComponent },
     { path: 'guild/:id/settings', component: GuildSettingsComponent },
     { path: 'guild/:id/statistics', component: GuildStatisticsComponent }
 ];
@@ -63,7 +61,6 @@ const routes: Routes = [
         AppComponent,
         HomeComponent,
         LoginComponent,
-        GuildHomeComponent,
         LoggedUserComponent,
         CommandsComponent,
         GuildSettingsComponent,
