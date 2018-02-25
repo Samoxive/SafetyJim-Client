34a3e14c7cb06f8751866fdd493b40b19bdd1c81
Şamil NART <samil.nart@gmail.com>
2018-02-25 13:53:01 +0300

    Update redirectUri of production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    redirectUri: 'https://safetyjim.xyz',
+    redirectUri: 'https://safetyjim.xyz/login',
     clientId: '313749262687141888',
     apiUrl: 'https://api.safetyjim.xyz'
 };
