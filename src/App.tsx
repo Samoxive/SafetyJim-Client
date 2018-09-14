e3d707a9784af8a05e45b37e87c17a0945fc44c1
Şamil NART <samil.nart@gmail.com>
2018-09-14 18:03:54 +0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Navbar';
+import { Commands } from './routes/commands/commands';
+import { Home } from './routes/home/home';
 
 class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                Hello
+            <div>
+                <Router>
+                    <>
+                        <Navbar />
+                        <Route exact={true} path="/" component={Home} />
+                        <Route path="/commands" component={Commands} />
+                    </>
+                </Router>
             </div>
         );
     }
