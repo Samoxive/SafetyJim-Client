902d437a9f4a0ac6d4ba53957277b28fb020f61f
Şamil NART <samil.nart@gmail.com>
2018-10-08 21:15:30 +0300

    Add authentication to dashboard

--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -1,22 +1,36 @@
-import { Modal } from 'antd';
+import { Icon, Modal, notification } from 'antd';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
-import { Redirect, RouteProps } from 'react-router';
+import { Redirect, Route, RouteComponentProps } from 'react-router';
+import { Guild } from '../../entities/guild';
 import { routeToOauth } from '../../environment';
 import { LoginStore } from '../../stores/loginStore';
+import { SelfUserStore } from '../../stores/selfUserStore';
+import { SettingsRoute } from './settings/settings';
 
-type DashboardProps = RouteProps & { loginStore?: LoginStore }
+type GenericGuildRouteProps  = {guild: Guild} & any;
 
-@inject('loginStore')
+function injectGuild(guild: Guild, Component: React.ComponentType<GenericGuildRouteProps>): React.StatelessComponent<any> {
+    return () => (<Component guild={guild} />);
+}
+
+type DashboardProps = RouteComponentProps<{ guildId: string }> & { loginStore?: LoginStore, selfUserStore?: SelfUserStore }
+
+@inject('loginStore', 'selfUserStore')
 @observer
-export class Dashboard extends React.Component<DashboardProps, { redirectHome: boolean }> {
+export class Dashboard extends React.Component<DashboardProps, { redirectHome: boolean, collapsed: boolean }> {
     state = {
         redirectHome: false,
+        collapsed: false,
     }
 
+    onCollapse = () => this.setState({ ...this.state, collapsed: !this.state.collapsed })
+
     render() {
-        const { loginStore, location } = this.props;
+        const { loginStore, selfUserStore, match, location } = this.props;
         const { redirectHome } = this.state;
+        const path = match.path;
+        const guildId = match.params.guildId;
 
         if (!loginStore!.token) {
             if (redirectHome) {
@@ -31,8 +45,25 @@ export class Dashboard extends React.Component<DashboardProps, { redirectHome: b
 
                 return (<></>);
             }
-        } else {
-            return (<div>Success!</div>);
         }
+
+        if (selfUserStore!.isLoading) {
+            return (<Icon type="loading" />);
+        }
+
+        const selectedGuild = selfUserStore!.self!.guilds.find(guild => guild.id === guildId)
+        if (selectedGuild == null) {
+            notification.error({
+                message: 'Invalid URL',
+                description: 'You have navigated to an invalid URL, redirecting...'
+            });
+            return <Redirect to="/" />
+        }
+
+        return (
+            <>
+                <Route path={`${path}/settings`} component={injectGuild(selectedGuild, SettingsRoute)} />
+            </>
+        );
     }
 }
