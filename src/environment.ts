5bf493788da64fc57b3b4522bab3635f15348dbe
Şamil NART <samil.nart@gmail.com>
2019-01-14 18:37:49 +0300

    Update static assets

--- src/environment.ts
+++ src/environment.ts
@@ -3,10 +3,10 @@ export const environment = process.env.NODE_ENV === 'production' ? {
     apiUrl: 'https://api.safetyjim.xyz',
     redirectUrl: 'https://safetyjim.xyz/login'
 } : {
-    clientId: '322714040344510464',
-    apiUrl: 'http://localhost:8080',
-    redirectUrl: 'http://localhost:3000/login'
-};
+        clientId: '322714040344510464',
+        apiUrl: 'http://localhost:8080',
+        redirectUrl: 'http://localhost:3000/login'
+    };
 
 export function routeToOauth(route?: string) {
     let url = `https://discordapp.com/api/oauth2/authorize?client_id=${environment.clientId}&redirect_uri=${encodeURIComponent(environment.redirectUrl)}&response_type=code&scope=identify`;
