3a518bd5fe3eedc121a95d1f218dae5ff085236e
Şamil NART <samil.nart@gmail.com>
2019-01-24 15:48:09 +0300

    Add help text to each settings group

--- src/routes/dashboard/settings/settings.tsx
+++ src/routes/dashboard/settings/settings.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Form, Icon, Input, InputNumber, Popconfirm, Select, Switch } from 'antd';
+import { Button, Card, Form, Icon, Input, InputNumber, Popconfirm, Select, Switch, Tooltip } from 'antd';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 import { Guild } from '../../../entities/guild';
@@ -9,6 +9,34 @@ import './settings.css';
 
 const TextArea = Input.TextArea;
 const Option = Select.Option;
+
+const INFO_TEXT: { [index: string]: string } = {
+    'modLog': 'Enabling this will log moderation actions like bans, kicks, warnings in given channel.',
+    'holdingRoom': 'Enabling this will assign the specified role to the new members after specified amount of time has passed. This role can be used to grant permissions to view exclusive channels, send messages and so on. Cannot be used while join captcha is enabled.',
+    'joinCaptcha': 'Enabling this will cause Jim to send a captcha challenge to new members to get the role specified in holding room setting. Cannot be used while holding room is enabled.',
+    'inviteLink': 'Enabling this will remove messages that contain any discord invite links, it will also kick new members that have an invite link as their username.',
+    'welcomeMessage': 'Enabling this will make Jim welcome new members by messaging in specified channel. The welcome message can be customized. Placeholders "$user" and "$guild" can be used to message member\'s and your server\'s name. If holding room is enabled, "$minute" placeholder can be used to specify the time left until they get their role assigned.',
+    'prefix': 'If no space prefix is enabled, Jim\'s commands can be issued without a space between command and the prefix (if prefix is set to "/", example usage will be "/help"). Prefix can also be customized.',
+    'silentCommands': 'If enabled, the command message user sent will be deleted if the command was a moderation action like ban, kick or warning.',
+    'statistics': 'Enabling this will allow Jim to collect statistics about your server. Jim never stores message contents. This setting isn\'t open to the public yet as it\'s a work-in-progress.'
+};
+
+const SettingsGroup = ({ children, title, infoKey }: { children: React.ReactChild[] | React.ReactChild, title: string, infoKey: string }) => {
+    const titleNode = (
+        <div style={{ display: 'flex' }}>
+            <div>{title}</div>
+            <Tooltip title={INFO_TEXT[infoKey]}>
+                <Icon type="info-circle" style={{ marginLeft: '8px', marginTop: '4px' }} />
+            </Tooltip>
+        </div>
+    );
+
+    return (
+        <Card title={titleNode} className="setting-group">
+            {children}
+        </Card>
+    );
+};
 
 interface SettingsRouteProps {
     guild: Guild;
@@ -65,7 +93,7 @@ export class SettingsRoute extends React.Component<SettingsRouteProps> {
             <>
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '16px', flexDirection: 'column', justifyItems: 'center', alignItems: 'center' }} >
                     <Form layout="inline" className="setting-groups" style={{ maxWidth: '1100px' }}>
-                        <Card title="Moderator Log" className="setting-group">
+                        <SettingsGroup title="Moderator Log" infoKey="modLog">
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onModlog} defaultChecked={settings.modLog} />
                             </Form.Item>
@@ -82,8 +110,8 @@ export class SettingsRoute extends React.Component<SettingsRouteProps> {
                                     }
                                 </Select>
                             </Form.Item>
-                        </Card>
-                        <Card title="Holding Room" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Holding Room" infoKey="holdingRoom">
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onHoldingRoom} defaultChecked={settings.holdingRoom} />
                             </Form.Item>
@@ -104,18 +132,18 @@ export class SettingsRoute extends React.Component<SettingsRouteProps> {
                             <Form.Item label="Minutes">
                                 <InputNumber onChange={this.onHoldingRoomMinutes} defaultValue={settings.holdingRoomMinutes} />
                             </Form.Item>
-                        </Card>
-                        <Card title="Join Captcha" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Join Captcha" infoKey="joinCaptcha">
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onJoinCaptcha} defaultChecked={settings.joinCaptcha} />
                             </Form.Item>
-                        </Card>
-                        <Card title="Invite Link Remover" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Invite Link Remover" infoKey={'inviteLink'}>
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onInviteLinkRemover} defaultChecked={settings.inviteLinkRemover} />
                             </Form.Item>
-                        </Card>
-                        <Card title="Welcome Messages" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Welcome Messages" infoKey="welcomeMessage">
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onWelcomeMessage} defaultChecked={settings.welcomeMessage} />
                             </Form.Item>
@@ -135,30 +163,30 @@ export class SettingsRoute extends React.Component<SettingsRouteProps> {
                             <Form.Item label="Message">
                                 <TextArea rows={1} onChange={this.onMessage} defaultValue={settings.message} />
                             </Form.Item>
-                        </Card>
-                        <Card title="Prefix Settings" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Prefix Settings" infoKey="prefix">
                             <Form.Item label="No Space Prefix Enable">
                                 <Switch onChange={this.onNoSpacePrefix} defaultChecked={settings.noSpacePrefix} />
                             </Form.Item>
                             <Form.Item label="Prefix">
                                 <Input onChange={this.onPrefix} defaultValue={settings.prefix} />
                             </Form.Item>
-                        </Card>
-                        <Card title="Silent Commands" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Silent Commands" infoKey="silentCommands">
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onSilentCommands} defaultChecked={settings.silentCommands} />
                             </Form.Item>
-                        </Card>
-                        <Card title="Statistics" className="setting-group">
+                        </SettingsGroup>
+                        <SettingsGroup title="Statistics" infoKey="statistics">
                             <Form.Item label="Enable">
                                 <Switch onChange={this.onStatistics} defaultChecked={settings.statistics} />
                             </Form.Item>
-                        </Card>
+                        </SettingsGroup>
                     </Form>
                     <div style={{ marginTop: '8px' }}>
                         <Button type="primary" onClick={this.onSubmit} style={{ marginLeft: '4px' }}>
                             Update
-                            </Button>
+                        </Button>
                         <Popconfirm
                             title="Are you sure you want to reset settings?"
                             onConfirm={this.onReset}
