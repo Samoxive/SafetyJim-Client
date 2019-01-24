ea9b3d5b4f4ab5a707812ac8839562c799f10547
Şamil NART <samil.nart@gmail.com>
2019-01-24 17:27:18 +0300

    Refactor settings route to use mobx observables properly

--- src/routes/dashboard/settings/settings.tsx
+++ src/routes/dashboard/settings/settings.tsx
@@ -1,6 +1,7 @@
 import { Button, Card, Form, Icon, Input, InputNumber, Popconfirm, Select, Switch, Tooltip } from 'antd';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
+import { GuildSettings } from 'src/entities/guildSettings';
 import { Guild } from '../../../entities/guild';
 import { GuildSettingsStore } from '../../../stores/guildSettingsStore';
 import { LoginStore } from '../../../stores/loginStore';
@@ -44,26 +45,144 @@ interface SettingsRouteProps {
     guildSettingsStore?: GuildSettingsStore;
 }
 
+@observer
+export class Settings extends React.Component<{ settings: GuildSettings, onSubmit: () => void, onReset: () => void }> {
+    constructor(props: { settings: GuildSettings, onSubmit: () => void, onReset: () => void }) {
+        super(props);
+        const c = console;
+        c.log('hello mate');
+    }
+    onModlog = (enabled: boolean) => this.props.settings.modLog = enabled;
+    onModLogChannel = (channelId: string) => this.props.settings.modLogChannel = this.props.settings.channels.find(channel => channel.id === channelId)!;
+    onHoldingRoom = (enabled: boolean) => this.props.settings.holdingRoom = enabled;
+    onHoldingRoomRole = (roleId?: string) => this.props.settings.holdingRoomRole = this.props.settings.roles.find(role => role.id === roleId);
+    onHoldingRoomMinutes = (minutes: number) => this.props.settings.holdingRoomMinutes = minutes;
+    onInviteLinkRemover = (enabled: boolean) => this.props.settings.inviteLinkRemover = enabled;
+    onWelcomeMessage = (enabled: boolean) => this.props.settings.welcomeMessage = enabled;
+    onMessage = (e: React.SyntheticEvent) => this.props.settings.message = (e.target as any).value as string;
+    onWelcomeMessageChannel = (channelId: string) => this.props.settings.welcomeMessageChannel = this.props.settings.channels.find(channel => channel.id === channelId)!;
+    onPrefix = (e: React.SyntheticEvent) => this.props.settings.prefix = (e.target as any).value as string;
+    onSilentCommands = (enabled: boolean) => this.props.settings.silentCommands = enabled;
+    onNoSpacePrefix = (enabled: boolean) => this.props.settings.noSpacePrefix = enabled;
+    onStatistics = (enabled: boolean) => this.props.settings.statistics = enabled;
+    onJoinCaptcha = (enabled: boolean) => this.props.settings.joinCaptcha = enabled;
+
+    render() {
+        const { settings, onSubmit, onReset } = this.props;
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '16px', flexDirection: 'column', justifyItems: 'center', alignItems: 'center' }} >
+                <Form layout="inline" className="setting-groups" style={{ maxWidth: '1100px' }}>
+                    <SettingsGroup title="Moderator Log" infoKey="modLog">
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onModlog} defaultChecked={settings.modLog} />
+                        </Form.Item>
+                        <Form.Item label="Channel">
+                            <Select
+                                onChange={this.onModLogChannel}
+                                defaultValue={settings.modLogChannel.name}
+                                style={{ width: '100px' }}
+                            >
+                                {
+                                    settings.channels.map(channel => (
+                                        <Option key={channel.id} value={channel.id}>{channel.name}</Option>
+                                    ))
+                                }
+                            </Select>
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Holding Room" infoKey="holdingRoom">
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onHoldingRoom} defaultChecked={settings.holdingRoom} />
+                        </Form.Item>
+                        <Form.Item label="Role">
+                            <Select
+                                onChange={this.onHoldingRoomRole as any}
+                                defaultValue={settings.holdingRoomRole ? settings.holdingRoomRole.id : ''}
+                                style={{ width: '100px' }}
+                            >
+                                <Option key={''} value={''} />
+                                {
+                                    settings.roles.map(role => (
+                                        <Option key={role.id} value={role.id}>{role.name}</Option>
+                                    ))
+                                }
+                            </Select>
+                        </Form.Item>
+                        <Form.Item label="Minutes">
+                            <InputNumber onChange={this.onHoldingRoomMinutes} defaultValue={settings.holdingRoomMinutes} />
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Join Captcha" infoKey="joinCaptcha">
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onJoinCaptcha} defaultChecked={settings.joinCaptcha} />
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Invite Link Remover" infoKey={'inviteLink'}>
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onInviteLinkRemover} defaultChecked={settings.inviteLinkRemover} />
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Welcome Messages" infoKey="welcomeMessage">
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onWelcomeMessage} defaultChecked={settings.welcomeMessage} />
+                        </Form.Item>
+                        <Form.Item label="Channel">
+                            <Select
+                                onChange={this.onWelcomeMessageChannel}
+                                defaultValue={settings.welcomeMessageChannel.name}
+                                style={{ width: '100px' }}
+                            >
+                                {
+                                    settings.channels.map(channel => (
+                                        <Option key={channel.id} value={channel.id}>{channel.name}</Option>
+                                    ))
+                                }
+                            </Select>
+                        </Form.Item>
+                        <Form.Item label="Message">
+                            <TextArea rows={1} onChange={this.onMessage} defaultValue={settings.message} />
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Prefix Settings" infoKey="prefix">
+                        <Form.Item label="No Space Prefix Enable">
+                            <Switch onChange={this.onNoSpacePrefix} defaultChecked={settings.noSpacePrefix} />
+                        </Form.Item>
+                        <Form.Item label="Prefix">
+                            <Input onChange={this.onPrefix} defaultValue={settings.prefix} />
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Silent Commands" infoKey="silentCommands">
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onSilentCommands} defaultChecked={settings.silentCommands} />
+                        </Form.Item>
+                    </SettingsGroup>
+                    <SettingsGroup title="Statistics" infoKey="statistics">
+                        <Form.Item label="Enable">
+                            <Switch onChange={this.onStatistics} defaultChecked={settings.statistics} />
+                        </Form.Item>
+                    </SettingsGroup>
+                </Form>
+                <div style={{ marginTop: '8px' }}>
+                    <Button type="primary" onClick={onSubmit} style={{ marginLeft: '4px' }}>
+                        Update
+                    </Button>
+                    <Popconfirm
+                        title="Are you sure you want to reset settings?"
+                        onConfirm={onReset}
+                        okText="Reset"
+                        cancelText="No"
+                    >
+                        <Button type="danger" style={{ marginLeft: '4px' }}>Reset</Button>
+                    </Popconfirm>
+                </div>
+            </div>
+        );
+    }
+}
+
 @inject('loginStore', 'guildSettingsStore')
 @observer
 export class SettingsRoute extends React.Component<SettingsRouteProps> {
-    getGuildState = () => this.props.guildSettingsStore!.guildSettingsGetter(this.props.guild.id)!
-
-    onModlog = (enabled: boolean) => this.getGuildState().modLog = enabled;
-    onModLogChannel = (channelId: string) => this.getGuildState().modLogChannel = this.getGuildState().channels.find(channel => channel.id === channelId)!;
-    onHoldingRoom = (enabled: boolean) => this.getGuildState().holdingRoom = enabled;
-    onHoldingRoomRole = (roleId?: string) => this.getGuildState().holdingRoomRole = this.getGuildState().roles.find(role => role.id === roleId);
-    onHoldingRoomMinutes = (minutes: number) => this.getGuildState().holdingRoomMinutes = minutes;
-    onInviteLinkRemover = (enabled: boolean) => this.getGuildState().inviteLinkRemover = enabled;
-    onWelcomeMessage = (enabled: boolean) => this.getGuildState().welcomeMessage = enabled;
-    onMessage = (e: React.SyntheticEvent) => this.getGuildState().message = (e.target as any).value as string;
-    onWelcomeMessageChannel = (channelId: string) => this.getGuildState().welcomeMessageChannel = this.getGuildState().channels.find(channel => channel.id === channelId)!;
-    onPrefix = (e: React.SyntheticEvent) => this.getGuildState().prefix = (e.target as any).value as string;
-    onSilentCommands = (enabled: boolean) => this.getGuildState().silentCommands = enabled;
-    onNoSpacePrefix = (enabled: boolean) => this.getGuildState().noSpacePrefix = enabled;
-    onStatistics = (enabled: boolean) => this.getGuildState().statistics = enabled;
-    onJoinCaptcha = (enabled: boolean) => this.getGuildState().joinCaptcha = enabled;
-
     onSubmit = () => {
         const { guild, loginStore, guildSettingsStore } = this.props;
         const settings = guildSettingsStore!.guildSettingsGetter(guild.id);
@@ -89,115 +208,6 @@ export class SettingsRoute extends React.Component<SettingsRouteProps> {
             return (<Icon type="loading" />);
         }
 
-        return (
-            <>
-                <div style={{ display: 'flex', justifyContent: 'center', marginTop: '16px', flexDirection: 'column', justifyItems: 'center', alignItems: 'center' }} >
-                    <Form layout="inline" className="setting-groups" style={{ maxWidth: '1100px' }}>
-                        <SettingsGroup title="Moderator Log" infoKey="modLog">
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onModlog} defaultChecked={settings.modLog} />
-                            </Form.Item>
-                            <Form.Item label="Channel">
-                                <Select
-                                    onChange={this.onModLogChannel}
-                                    defaultValue={settings.modLogChannel.name}
-                                    style={{ width: '100px' }}
-                                >
-                                    {
-                                        settings.channels.map(channel => (
-                                            <Option key={channel.id} value={channel.id}>{channel.name}</Option>
-                                        ))
-                                    }
-                                </Select>
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Holding Room" infoKey="holdingRoom">
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onHoldingRoom} defaultChecked={settings.holdingRoom} />
-                            </Form.Item>
-                            <Form.Item label="Role">
-                                <Select
-                                    onChange={this.onHoldingRoomRole as any}
-                                    defaultValue={settings.holdingRoomRole ? settings.holdingRoomRole.id : ''}
-                                    style={{ width: '100px' }}
-                                >
-                                    <Option key={''} value={''} />
-                                    {
-                                        settings.roles.map(role => (
-                                            <Option key={role.id} value={role.id}>{role.name}</Option>
-                                        ))
-                                    }
-                                </Select>
-                            </Form.Item>
-                            <Form.Item label="Minutes">
-                                <InputNumber onChange={this.onHoldingRoomMinutes} defaultValue={settings.holdingRoomMinutes} />
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Join Captcha" infoKey="joinCaptcha">
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onJoinCaptcha} defaultChecked={settings.joinCaptcha} />
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Invite Link Remover" infoKey={'inviteLink'}>
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onInviteLinkRemover} defaultChecked={settings.inviteLinkRemover} />
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Welcome Messages" infoKey="welcomeMessage">
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onWelcomeMessage} defaultChecked={settings.welcomeMessage} />
-                            </Form.Item>
-                            <Form.Item label="Channel">
-                                <Select
-                                    onChange={this.onWelcomeMessageChannel}
-                                    defaultValue={settings.welcomeMessageChannel.name}
-                                    style={{ width: '100px' }}
-                                >
-                                    {
-                                        settings.channels.map(channel => (
-                                            <Option key={channel.id} value={channel.id}>{channel.name}</Option>
-                                        ))
-                                    }
-                                </Select>
-                            </Form.Item>
-                            <Form.Item label="Message">
-                                <TextArea rows={1} onChange={this.onMessage} defaultValue={settings.message} />
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Prefix Settings" infoKey="prefix">
-                            <Form.Item label="No Space Prefix Enable">
-                                <Switch onChange={this.onNoSpacePrefix} defaultChecked={settings.noSpacePrefix} />
-                            </Form.Item>
-                            <Form.Item label="Prefix">
-                                <Input onChange={this.onPrefix} defaultValue={settings.prefix} />
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Silent Commands" infoKey="silentCommands">
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onSilentCommands} defaultChecked={settings.silentCommands} />
-                            </Form.Item>
-                        </SettingsGroup>
-                        <SettingsGroup title="Statistics" infoKey="statistics">
-                            <Form.Item label="Enable">
-                                <Switch onChange={this.onStatistics} defaultChecked={settings.statistics} />
-                            </Form.Item>
-                        </SettingsGroup>
-                    </Form>
-                    <div style={{ marginTop: '8px' }}>
-                        <Button type="primary" onClick={this.onSubmit} style={{ marginLeft: '4px' }}>
-                            Update
-                        </Button>
-                        <Popconfirm
-                            title="Are you sure you want to reset settings?"
-                            onConfirm={this.onReset}
-                            okText="Reset"
-                            cancelText="No"
-                        >
-                            <Button type="danger" style={{ marginLeft: '4px' }}>Reset</Button>
-                        </Popconfirm>
-                    </div>
-                </div>
-            </>
-        );
+        return (<Settings settings={guildSettingsStore!.settings.get(guild.id)!} onSubmit={this.onSubmit} onReset={this.onReset} />)
     }
 }
