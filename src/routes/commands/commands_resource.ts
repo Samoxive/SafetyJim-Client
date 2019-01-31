d75310634659848f32df6fd4f27e48ccd394dcfc
Şamil NART <samil.nart@gmail.com>
2019-01-31 14:01:35 +0300

    Add updated information about mentioning users in commands, add hardban, invite, info, iam commands

--- src/routes/commands/commands_resource.ts
+++ src/routes/commands/commands_resource.ts
@@ -18,11 +18,32 @@ export const commands: Command[] = [
             'the ban duration by using the seperator "|" and typing the duration after.',
         usages: [
             '-mod ban @user', '-mod ban @user <reason>',
-            '-mod ban @user <reason> | <time>'
+            '-mod ban @user <reason> | <time>',
+            '-mod ban <username> <reason> | <time>',
+            '-mod ban <user_id> <reason> | <time>'
         ],
         examples: [
             '-mod ban @Samoxive#1234', '-mod ban @Samoxive#1234 Spamming',
-            '-mod ban @Samoxive#1234 Messaging invite links | 1 day'
+            '-mod ban @Samoxive#1234 Messaging invite links | 1 day',
+            '-mod ban Samox | 3 days', '-mod ban 145456746721312768'
+        ]
+    },
+    {
+        name: 'Hardban',
+        tooltip: 'Hardbans specified user, deleting their messages',
+        quick: '-mod hardban @Samoxive#1234 Spam',
+        description:
+            'Hardbans the mentioned user, banning them and deleting their messages in the last week. ' +
+            'If a text is given after the mention, that text is used as the hardban reason.',
+        usages: [
+            '-mod hardban @user', '-mod hardban @user <reason>',
+            '-mod hardban <username> <reason>',
+            '-mod hardban <user_id> <reason>'
+        ],
+        examples: [
+            '-mod hardban @Samoxive#1234',
+            '-mod hardban @Samoxive#1234 Spamming', '-mod hardban Samox',
+            '-mod hardban 145456746721312768'
         ]
     },
     {
@@ -32,10 +53,15 @@ export const commands: Command[] = [
         description:
             'Kicks the mentioned user, if a text is given after the mention, ' +
             'that text is used as the kick reason.',
-        usages: ['-mod kick @user', '-mod kick @user <reason>'],
+        usages: [
+            '-mod kick @user', '-mod kick @user <reason>',
+            '-mod kick <username> <reason>', '-mod kick <user_id> <reason>'
+        ],
         examples: [
             '-mod kick @Samoxive#1234',
-            '-mod kick @Samoxive#1234 Not abiding by the rules'
+            '-mod kick @Samoxive#1234 Not abiding by the rules',
+            '-mod kick Samoxive For being samoxive',
+            '-mod kick 145456746721312768'
         ]
     },
     {
@@ -48,13 +74,16 @@ export const commands: Command[] = [
             'after the mention, that text is used as the kick reason. To mute a user temporarily, you can ' +
             'specify the mute duraction by using the seperator "|" and typing the duration after.',
         usages: [
-            '-mod mute @user',
-            '-mod mute @user <reason>',
+            '-mod mute @user', '-mod mute @user <reason>',
             '-mod mute @user <reason> | <time>',
+            '-mod mute <username> <reason> | <time>',
+            '-mod mute <user_id> <reason> | <time>'
         ],
         examples: [
             '-mod mute @Samoxive#1234', '-mod mute @Samoxive#1234 Spamming',
-            '-mod mute @Samoxive#1234 Playing loud music | 6 hours'
+            '-mod mute @Samoxive#1234 Playing loud music | 6 hours',
+            '-mod mute Sam Talking too much | 1 hour',
+            '-mod mute 145456746721312768 Spamming the chat'
         ]
     },
     {
@@ -66,12 +95,14 @@ export const commands: Command[] = [
             'received a warning (you can customize it by specifying a reason). If a text is given after ' +
             'the mention, that text is used as the warning reason.',
         usages: [
-            '-mod warn @user',
-            '-mod warn @user <reason>',
+            '-mod warn @user', '-mod warn @user <reason>',
+            '-mod warn <username> <reason>', '-mod warn <user_id> <reason>'
         ],
         examples: [
             '-mod warn @Samoxive#1234',
-            '-mod warn @Samoxive#1234 Selfbots are not allowed'
+            '-mod warn @Samoxive#1234 Selfbots are not allowed',
+            '-mod warn Samo Your username is too long',
+            '-mod warn 145456746721312768'
         ],
     },
     {
@@ -79,19 +110,22 @@ export const commands: Command[] = [
         tooltip: 'Softbans the specified user',
         quick: '-mod softban @Samoxive#1234 Spamming nsfw pictures',
         description:
-            'Softbans the mentioned user, banning and unbanning them afterwards. This deletes ' +
+            'Softbans the mentioned user, banning and unbanning them afterwards, effectively kicking them. This deletes ' +
             'the specified user\'s messages in the last x days (x is 1 by default, can be 7 at maximum). ' +
             'If a text is given after the mention, that text is used as the softban reason. To customize ' +
             'the day count of messages deleted, you can use the "|" seperator and specify the number afterwards.',
         usages: [
-            '-mod softban @user',
-            '-mod softban @user <reason>',
+            '-mod softban @user', '-mod softban @user <reason>',
             '-mod softban @user <reason> | <number of days to delete messages>',
+            '-mod softban <username> <reason> | <number of days to delete messages>',
+            '-mod softban <user_id> <reason> | <number of days to delete messages>'
         ],
         examples: [
             '-mod softban @Samoxive#1234',
             '-mod softban @Samoxive#1234 Spamming',
-            '-mod softban @Samoxive#1234 Spamming | 3'
+            '-mod softban @Samoxive#1234 Spamming | 3',
+            '-mod softban Samoxive For reasons unknown',
+            '-mod softban 145456746721312768'
         ]
     },
     {
@@ -114,13 +148,32 @@ export const commands: Command[] = [
     },
     {
         name: 'Server',
-        tooltip: 'Display information about the current server',
+        tooltip: 'Display information about the discord server',
         quick: '-mod server',
         description:
             'By using this command, you can view the creation date of the server, ' +
             'the owner, member count and the emojis available.',
         usages: ['-mod server'],
         examples: ['-mod server']
+    },
+    {
+        name: 'Invite',
+        tooltip: 'Display invite links for Jim',
+        quick: '-mod invite',
+        description:
+            'By using this command, you can view the invite link of Jim and the support server.',
+        usages: ['-mod invite'],
+        examples: ['-mod invite']
+    },
+    {
+        name: 'Info',
+        tooltip: 'Display statistics and links about Jim',
+        quick: '-mod info',
+        description:
+            'By using this command, you can view some statistics about Jim, the uptime, diagnostics information ' +
+            'and links to support Jim and get support.',
+        usages: ['-mod info'],
+        examples: ['-mod info']
     },
     {
         name: 'Role',
@@ -131,8 +184,18 @@ export const commands: Command[] = [
             'self-assignable role, available for every to self-assign via the `Iam` command. To add a role, a ' +
             'role with given name must exist. You can also use ' +
             'the `remove` subcommand to get rid of a self-assignable role.',
-        usages: ['-mod role add <roleName>', '-mod role remove <roleName>'],
+        usages: ['-mod role add <role_name>', '-mod role remove <role_name>'],
         examples: ['-mod role add No-NSFW', '-mod role remove No-NSFW']
+    },
+    {
+        name: 'Iam',
+        tooltip: 'Self-assigns a role',
+        quick: '-mod iam AFK',
+        description:
+            'Self-assigns the specified role, the specified role must be added by an administrator ' +
+            'to the self-assignable roles list by using the `role add` command.',
+        usages: ['-mod iam <role_name>'],
+        examples: ['-mod iam AFK']
     },
     {
         name: 'Ping',
@@ -154,10 +217,13 @@ export const commands: Command[] = [
             'never clear the original command message.',
         usages: [
             '-mod clean <number>', '-mod clean <number> bot',
-            '-mod clean <number> @user'
+            '-mod clean <number> @user', '-mod clean <number> <username>',
+            '-mod clean <number> <user_id>'
         ],
-        examples:
-            ['-mod clean 5', '-mod clean 2 bot', '-mod clean 10 @Samoxive#1234']
+        examples: [
+            '-mod clean 5', '-mod clean 2 bot', '-mod clean 10 @Samoxive#1234',
+            '-mod clean 50 Samoxi', '-mod tag 3 145456746721312768'
+        ]
     },
     {
         name: 'Remind',
@@ -177,13 +243,14 @@ export const commands: Command[] = [
         name: 'Unban',
         tooltip: 'Unbans specified user',
         quick: '-mod unban Samoxive#1234',
-        description:
-            'Unbans the specified user, due to limitiations of discord, ' +
-            'a name and discriminator is needed instead of a user mention.',
+        description: 'Unbans the specified user.',
         usages: [
-            '-mod unban user#discriminator',
+            '-mod unban @user', '-mod unban <username>', '-mod unban <user_id>'
         ],
-        examples: ['-mod unban Samoxive#1234']
+        examples: [
+            '-mod unban @Samoxive#1234', '-mod unban samox',
+            '-mod unban 145456746721312768'
+        ]
     },
     {
         name: 'Unmute',
@@ -191,8 +258,14 @@ export const commands: Command[] = [
         quick: '-mod unban @Samoxive#1234',
         description:
             'Unmutes specified user, removing the Muted role from them.',
-        usages: ['-mod unmute @user'],
-        examples: ['-mod unmute @Samoxive#1234']
+        usages: [
+            '-mod unmute @user', '-mod unmute <username>',
+            '-mod unmute <user_id>'
+        ],
+        examples: [
+            '-mod unmute @Samoxive#1234', '-mod unmute melo',
+            '-mod unmute 129819557115199488'
+        ]
     },
     {
         name: 'Tag',
