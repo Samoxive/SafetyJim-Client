f4fa903744773face287e7ccbd9ddc8cfb2404c7
Şamil NART <samil.nart@gmail.com>
2019-01-31 23:26:21 +0300

    Sort out home page

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,3 +1,28 @@
+import { Button } from 'antd';
 import * as React from 'react';
+import Jimbo from '../../assets/jimbo.png';
+import './home.css';
 
-export const Home = () => (<div>hai</div>);
+const inviteLink = 'https://discordapp.com/oauth2/authorize?client_id=313749262687141888&permissions=272706646&scope=bot';
+const supportLink = 'https://discord.io/safetyjim';
+const donateLink = 'https://www.patreon.com/safetyjim';
+
+export const Home = () => (
+    <div className="home-center">
+        <img className="home-jim-logo" src={Jimbo} />
+        <h2 className="home-jim-desc">
+            Jim is a hardworking bot who will help you with your moderation and server management needs.
+        </h2>
+        <div className="home-buttons">
+            <Button type="primary" ghost={true} className="home-button" href={inviteLink}>
+                Invite Jim!
+            </Button>
+            <Button type="primary" ghost={true} className="home-button" href={supportLink}>
+                Support
+            </Button>
+            <Button type="primary" ghost={true} className="home-button" href={donateLink}>
+                Donate
+            </Button>
+        </div>
+    </div>
+);
