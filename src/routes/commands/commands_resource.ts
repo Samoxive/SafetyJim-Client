95b0bcb5a6f5db05c54054bd2fedb4ad112e1683
Starwort <tcphone93@gmail.com>
2019-03-20 19:51:37 +0000

    Fix spelling, grammar, modify phrasing for more clarity
    Spelling was mostly 'seperator' -> 'separator'
    Grammar was things like 'a text', etc.
    Phrasing was the large bulk of the edits.

--- src/routes/commands/commands_resource.ts
+++ src/routes/commands/commands_resource.ts
@@ -10,88 +10,88 @@ interface Command {
 export const commands: Command[] = [
     {
         name: "Ban",
-        tooltip: "Bans specified user",
+        tooltip: "Bans the specified user but does not delete their recent messages",
         quick: "-mod ban @Samoxive#1234 Spam | 3 days",
         description:
-            "Bans the mentioned user, if a text is given after the mention, " +
-            "that text is used as the ban reason. To ban a user temporarily, you can specify " +
-            'the ban duration by using the seperator "|" and typing the duration after.',
+            "Bans the specified user, if additional text follows the identifier, " +
+            "that text is used as the reason. To ban a user temporarily, you can specify " +
+            'a duration by using the separator "|", and then typing the duration in full.',
         usages: [
             "-mod ban @user",
             "-mod ban @user <reason>",
-            "-mod ban @user <reason> | <time>",
-            "-mod ban <username> <reason> | <time>",
-            "-mod ban <user_id> <reason> | <time>"
+            "-mod ban @user [reason] | <time>",
+            "-mod ban <username> [reason] [| <time>]",
+            "-mod ban <user_id> [reason] [| <time>]"
         ],
         examples: [
             "-mod ban @Samoxive#1234",
             "-mod ban @Samoxive#1234 Spamming",
-            "-mod ban @Samoxive#1234 Messaging invite links | 1 day",
-            "-mod ban Samox | 3 days",
+            "-mod ban @Samoxive#1234 Posting invite links | 1 day",
+            "-mod ban Samoxi | 3 days",
             "-mod ban 145456746721312768"
         ]
     },
     {
         name: "Hardban",
-        tooltip: "Hardbans specified user, deleting their messages",
+        tooltip: "Bans the specified user and deletes their recent messages",
         quick: "-mod hardban @Samoxive#1234 Spam",
         description:
-            "Hardbans the mentioned user, banning them and deleting their messages in the last week. " +
-            "If a text is given after the mention, that text is used as the hardban reason.",
+            "Bans the specified user, banning them and deleting every message they sent in the last week. " +
+            "The syntax is equivalent to that of the command `ban`.",
         usages: [
             "-mod hardban @user",
             "-mod hardban @user <reason>",
-            "-mod hardban <username> <reason>",
-            "-mod hardban <user_id> <reason>"
+            "-mod hardban <username> [reason]",
+            "-mod hardban <user_id> [reason]"
         ],
         examples: [
             "-mod hardban @Samoxive#1234",
             "-mod hardban @Samoxive#1234 Spamming",
             "-mod hardban Samox",
-            "-mod hardban 145456746721312768"
+            "-mod hardban 145456746721312768 Bad Samoxive no cookie"
         ]
     },
     {
         name: "Kick",
-        tooltip: "Kicks specified user",
+        tooltip: "Kicks the specified user",
         quick: "-mod kick @Samoxive#1234 Advertisement",
         description:
-            "Kicks the mentioned user, if a text is given after the mention, " +
-            "that text is used as the kick reason.",
+            "Kicks the specified user, if additional text follows the identifier, " +
+            "that text is used as the reason.",
         usages: [
             "-mod kick @user",
             "-mod kick @user <reason>",
-            "-mod kick <username> <reason>",
-            "-mod kick <user_id> <reason>"
+            "-mod kick <username> [reason]",
+            "-mod kick <user_id> [reason]"
         ],
         examples: [
             "-mod kick @Samoxive#1234",
-            "-mod kick @Samoxive#1234 Not abiding by the rules",
-            "-mod kick Samoxive For being samoxive",
+            "-mod kick @Samoxive#1234 Breaking the rules",
+            "-mod kick Sam For being samoxive",
             "-mod kick 145456746721312768"
         ]
     },
     {
         name: "Mute",
-        tooltip: "Mutes specified user",
+        tooltip: "Mutes the specified user",
         quick: "-mod mute @Samoxive#1234 Using profanity",
         description:
-            "Mutes the mentioned user, this means user can't send messages in text channels, " +
-            "they can't use their microphone in voice channels while they are muted. If a text is given " +
-            "after the mention, that text is used as the kick reason. To mute a user temporarily, you can " +
-            'specify the mute duraction by using the seperator "|" and typing the duration after.',
+            "Mutes the specified user. While muted, a user cannot send messages in any text channel, or " +
+            "use their microphone in any voice channel. If additional text follows the identifier, " +
+            "that text is used as the reason. To mute a user temporarily, you can " +
+            'specify the duration by using the separator "|", and then typing the duration in full.',
         usages: [
             "-mod mute @user",
             "-mod mute @user <reason>",
-            "-mod mute @user <reason> | <time>",
-            "-mod mute <username> <reason> | <time>",
-            "-mod mute <user_id> <reason> | <time>"
+            "-mod mute @user [reason] | <time>",
+            "-mod mute <username> [reason] [| <time>]",
+            "-mod mute <user_id> [reason] [| <time>]"
         ],
         examples: [
             "-mod mute @Samoxive#1234",
             "-mod mute @Samoxive#1234 Spamming",
             "-mod mute @Samoxive#1234 Playing loud music | 6 hours",
-            "-mod mute Sam Talking too much | 1 hour",
+            "-mod mute Samoxive | 1 hour",
             "-mod mute 145456746721312768 Spamming the chat"
         ]
     },
@@ -100,14 +100,13 @@ export const commands: Command[] = [
         tooltip: "Warns the specified user",
         quick: "-mod warn @Samoxive#1234 Harassing users",
         description:
-            "Warns the mentioned user, sending them a direct message that explains why they " +
-            "received a warning (you can customize it by specifying a reason). If a text is given after " +
-            "the mention, that text is used as the warning reason.",
+            "Warns the specified user, sending them a direct message including the reason specified, if any. " +
+            "Any text following the identifier is used as the reason.",
         usages: [
             "-mod warn @user",
             "-mod warn @user <reason>",
-            "-mod warn <username> <reason>",
-            "-mod warn <user_id> <reason>"
+            "-mod warn <username> [reason]",
+            "-mod warn <user_id> [reason]"
         ],
         examples: [
             "-mod warn @Samoxive#1234",
@@ -118,26 +117,26 @@ export const commands: Command[] = [
     },
     {
         name: "Softban",
-        tooltip: "Softbans the specified user",
+        tooltip: "Bans, then unbans, the specified user, deleting their recent messages",
         quick: "-mod softban @Samoxive#1234 Spamming nsfw pictures",
         description:
-            "Softbans the mentioned user, banning and unbanning them afterwards, effectively kicking them. This deletes " +
-            "the specified user's messages in the last x days (x is 1 by default, can be 7 at maximum). " +
-            "If a text is given after the mention, that text is used as the softban reason. To customize " +
-            'the day count of messages deleted, you can use the "|" seperator and specify the number afterwards.',
+            "Bans the specified user, then unbans them afterwards, effectively kicking them. This deletes " +
+            "the specified user's messages in the last 1-7 days, default 1. " +
+            "If a text is given after the identifier, that text is used as the reason. To customise " +
+            'the number of days from which to delete messages, you can use the separator "|", followed by a number.',
         usages: [
             "-mod softban @user",
             "-mod softban @user <reason>",
-            "-mod softban @user <reason> | <number of days to delete messages>",
-            "-mod softban <username> <reason> | <number of days to delete messages>",
-            "-mod softban <user_id> <reason> | <number of days to delete messages>"
+            "-mod softban @user [reason] | <number of days to delete messages>",
+            "-mod softban <username> [reason] [| <number of days to delete messages>]",
+            "-mod softban <user_id> [reason] [| <number of days to delete messages>]"
         ],
         examples: [
             "-mod softban @Samoxive#1234",
             "-mod softban @Samoxive#1234 Spamming",
             "-mod softban @Samoxive#1234 Spamming | 3",
             "-mod softban Samoxive For reasons unknown",
-            "-mod softban 145456746721312768"
+            "-mod softban 145456746721312768 | 6"
         ]
     },
     {
@@ -146,9 +145,9 @@ export const commands: Command[] = [
         quick: "-mod settings set InviteLinkRemover enabled",
         description:
             "This command has multiple subcommands, to view current state of server wide settings, " +
-            "use the `display` subcommand. To change settings, you can use the subcommand `set` with the relevant " +
-            "setting key. To view the setting keys you can use and the possible values for them, use the `list` command. " +
-            "You can also use the subcommand `reset` with no arguments to revert every setting to the default values.",
+            "use the subcommand `display`. To change settings, you can use the subcommand `set` with a setting's key " +
+            "and its new value. To view the settings' keys, and their possible values, use the subcommand `list`. " +
+            "You can also use the subcommand `reset`, with no arguments, to revert every setting to its default value.",
         usages: [
             "-mod settings display",
             "-mod settings list",
@@ -167,27 +166,27 @@ export const commands: Command[] = [
         tooltip: "Display information about the discord server",
         quick: "-mod server",
         description:
-            "By using this command, you can view the creation date of the server, " +
-            "the owner, member count and the emojis available.",
+            "This command provides the creation date of the server, " +
+            "the owner, member count and the available emoji.",
         usages: ["-mod server"],
         examples: ["-mod server"]
     },
     {
         name: "Invite",
-        tooltip: "Display invite links for Jim",
+        tooltip: "Display links to invite Jim",
         quick: "-mod invite",
         description:
-            "By using this command, you can view the invite link of Jim and the support server.",
+            "This command provides the link to invite Jim, and the support server link.",
         usages: ["-mod invite"],
         examples: ["-mod invite"]
     },
     {
         name: "Info",
-        tooltip: "Display statistics and links about Jim",
+        tooltip: "Display statistics about, and links related to, Jim",
         quick: "-mod info",
         description:
-            "By using this command, you can view some statistics about Jim, the uptime, diagnostics information " +
-            "and links to support Jim and get support.",
+            "This command shows some statistics about Jim, including the uptime and diagnostics information, " +
+            "as well as links to support Jim, invite Jim, and get support.",
         usages: ["-mod info"],
         examples: ["-mod info"]
     },
@@ -196,10 +195,10 @@ export const commands: Command[] = [
         tooltip: "Create self-assignable roles",
         quick: "-mod role add AFK",
         description:
-            "This command has multiple subcommands, you can use the `add` subcommand to create a new " +
-            "self-assignable role, available for every to self-assign via the `Iam` command. To add a role, a " +
-            "role with given name must exist. You can also use " +
-            "the `remove` subcommand to get rid of a self-assignable role.",
+            "This command has multiple subcommands, you can use the subcommand `add` to create a new " +
+            "self-assignable role, available for anyone to claim by using the command `Iam`. To add a role, a " +
+            "role with the given name must exist. You can also use " +
+            "the subcommand `remove` to make a role no longer self-assignable.",
         usages: ["-mod role add <role_name>", "-mod role remove <role_name>"],
         examples: ["-mod role add No-NSFW", "-mod role remove No-NSFW"]
     },
@@ -208,8 +207,8 @@ export const commands: Command[] = [
         tooltip: "Self-assigns a role",
         quick: "-mod iam AFK",
         description:
-            "Self-assigns the specified role, the specified role must be added by an administrator " +
-            "to the self-assignable roles list by using the `role add` command.",
+            "Self-assigns the specified role, the specified role must have been added " +
+            "to the self-assignable roles list by an administrator who used the command `role add`.",
         usages: ["-mod iam <role_name>"],
         examples: ["-mod iam AFK"]
     },
@@ -218,19 +217,19 @@ export const commands: Command[] = [
         tooltip: "Pongs if Jim is alive",
         quick: "-mod ping",
         description:
-            "This command is used to check if Jim is alive, it also displays websocket ping to the Discord API.",
+            "This command is used to check if Jim is alive. If he is, he displays his websocket ping to the Discord API.",
         usages: ["-mod ping"],
         examples: ["-mod ping"]
     },
     {
         name: "Clean",
-        tooltip: "Bulk deletes messages",
+        tooltip: "Deletes messages in bulk",
         quick: "-mod clean 3",
         description:
-            "Cleans amount of messages specified by the user, moderators can use two more methods to specialize the " +
-            "cleaning process. By specifying the word `bot` after the amount, you can use Jim to clear only bot messages. By sp" +
-            "ecifying a user mention after the amount, you can use Jim to clear messages only from a certain user. This command will " +
-            "never clear the original command message.",
+            "Deletes the specified number of messages from the current channel. You may specialise the cleaning process " +
+            "in two ways: By specifying the word `bot` after the amount, you can use Jim to clear only bot messages. Alternatively, " +
+            "by specifying a user after the amount, you can use Jim to clear messages only from a certain user. This command will " +
+            "not clear the original command message.",
         usages: [
             "-mod clean <number>",
             "-mod clean <number> bot",
@@ -243,17 +242,17 @@ export const commands: Command[] = [
             "-mod clean 2 bot",
             "-mod clean 10 @Samoxive#1234",
             "-mod clean 50 Samoxi",
-            "-mod tag 3 145456746721312768"
+            "-mod clean 3 145456746721312768"
         ]
     },
     {
         name: "Remind",
-        tooltip: "Sets a reminder for a future date",
+        tooltip: "Sets a reminder for a date in the future",
         quick: "-mod remind Buy groceries",
         description:
-            "Sets a reminder to send you a message after the specified amount of time (if unspecified, it defaults to a day) " +
-            "that contains the specified piece of message. If private messages are blocked, " +
-            "Jim will remind in the channel reminder was set in.",
+            "Sets a reminder to send you a message after the specified amount of time, or one day if unspecified, " +
+            "that contains the reminder you set. If private messages are blocked, " +
+            "Jim will post the reminder in the channel in which it was set.",
         usages: ["-mod remind <message>", "-mod remind <message> | <time>"],
         examples: [
             "-mod remind Do the taxes",
@@ -262,7 +261,7 @@ export const commands: Command[] = [
     },
     {
         name: "Unban",
-        tooltip: "Unbans specified user",
+        tooltip: "Unbans the specified user",
         quick: "-mod unban Samoxive#1234",
         description: "Unbans the specified user.",
         usages: [
@@ -278,10 +277,10 @@ export const commands: Command[] = [
     },
     {
         name: "Unmute",
-        tooltip: "Unmutes specified user",
+        tooltip: "Unmutes the specified user",
         quick: "-mod unban @Samoxive#1234",
         description:
-            "Unmutes specified user, removing the Muted role from them.",
+            "Unmutes the specified user; removing the Muted role from them.",
         usages: [
             "-mod unmute @user",
             "-mod unmute <username>",
@@ -289,18 +288,18 @@ export const commands: Command[] = [
         ],
         examples: [
             "-mod unmute @Samoxive#1234",
-            "-mod unmute melo",
+            "-mod unmute Samoxive#1234",
             "-mod unmute 129819557115199488"
         ]
     },
     {
         name: "Tag",
-        tooltip: "Makes Jim repeat a pre-created response",
+        tooltip: "Creates, manages, and repeats textual shortcuts",
         quick: "-mod tag rule1",
         description:
             "When a tag name is given, Jim repeats the response associated with the name. You can use the subcommand `add` to " +
-            "create a new tag, you can use the subcommand `remove` to delete an existing tag, you can use the subcommand `edit` to " +
-            "change the response that the given tag name is associated with. To display the existing tag names, " +
+            "create a new tag, you can use the subcommand `remove` to delete an existing tag, or you can use the subcommand `edit` to " +
+            "change the response associated with the given tag. To display all of the tags, " +
             "you can use the subcommand `list`.",
         usages: [
             "-mod tag list",
@@ -313,7 +312,7 @@ export const commands: Command[] = [
             "-mod tag list",
             "-mod tag rule1",
             "-mod tag add rule1 No advertising.",
-            "-mod tag edit rule1 No advertising unless a permission is given by the moderators.",
+            "-mod tag edit rule1 No advertising without a moderator's permission.",
             "-mod tag remove rule1"
         ]
     }
