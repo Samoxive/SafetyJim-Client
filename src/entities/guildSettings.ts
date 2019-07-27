65f64718c1649f2b86d27e2143c5d55748ae3279
Şamil NART <samil.nart@gmail.com>
2019-07-27 23:24:26 +0300

    Move enum constants from server code

--- src/entities/guildSettings.ts
+++ src/entities/guildSettings.ts
@@ -2,6 +2,24 @@ import { Channel } from "./channel";
 import { Guild } from "./guild";
 import { Role } from "./role";
 
+export const GuildSettingsConstants = {
+    SILENT_COMMANDS_MOD_ONLY: 0,
+    SILENT_COMMANDS_ALL: 1,
+    WORD_FILTER_LEVEL_LOW: 0,
+    WORD_FILTER_LEVEL_HIGH: 1,
+    ACTION_NOTHING: 0,
+    ACTION_WARN: 1,
+    ACTION_MUTE: 2,
+    ACTION_KICK: 3,
+    ACTION_BAN: 4,
+    ACTION_SOFTBAN: 5,
+    ACTION_HARDBAN: 6,
+    DURATION_TYPE_SECONDS: 0,
+    DURATION_TYPE_MINUTES: 1,
+    DURATION_TYPE_HOURS: 2,
+    DURATION_TYPE_DAYS: 3
+};
+
 export interface GuildSettings {
     guild: Guild;
     channels: Channel[];
@@ -22,4 +40,10 @@ export interface GuildSettings {
     joinCaptcha: boolean;
     silentCommandsLevel: number;
     modActionConfirmationMessage: boolean;
+    wordFilter: boolean;
+    wordFilterBlacklist: string | null;
+    wordFilterLevel: number;
+    wordFilterAction: number;
+    wordFilterActionDuration: number;
+    wordFilterActionDurationType: number;
 }
