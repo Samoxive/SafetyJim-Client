2b5d870bd93e84cc95f834edf6a1a7ed63b9fc24
Şamil NART <samil.nart@gmail.com>
2019-10-22 14:49:22 +0300

    Fix user id tooltip not working properly

--- src/components/user_text.tsx
+++ src/components/user_text.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { User } from "../entities/user";
-import { OverlayTrigger, Tooltip, Button } from "react-bootstrap";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 type UserTextProps = {
     user: User;
@@ -12,6 +12,6 @@ export const UserText = ({ user }: UserTextProps) => (
             <Tooltip id={user.id + Math.random()}>{`ID: ${user.id}`}</Tooltip>
         }
     >
-        {user.username}
+        <span>{user.username}</span>
     </OverlayTrigger>
 );
