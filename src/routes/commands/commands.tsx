d1fdd29ca6f3368b8f7a8ce7d591ff631fa533f5
Şamil NART <samil.nart@gmail.com>
2021-02-27 22:38:47 +0300

    Use accordion instead of popover elements for command usages/examples to fix mobile browser

--- src/routes/commands/commands.tsx
+++ src/routes/commands/commands.tsx
@@ -1,4 +1,4 @@
-import { Card, Popover, OverlayTrigger, Button } from "react-bootstrap";
+import { Card, Button, Accordion } from "react-bootstrap";
 import * as React from "react";
 import "./commands.css";
 import { commands } from "./commands_resource";
@@ -12,28 +12,50 @@ export const Commands = () => (
         />
         {commands
             .sort((a, b) => a.name.localeCompare(b.name))
-            .map(command => {
-                const usagesOverlay = (
-                    <Popover id={`usages-popover-${command.name}`}>
-                        <ul>
-                            {command.usages.map((usage, i) => (
-                                <li key={i}>
-                                    <code>{usage}</code>
-                                </li>
-                            ))}
-                        </ul>
-                    </Popover>
+            .map((command) => {
+                const usagesAccordion = (
+                    <Accordion>
+                        <Accordion.Toggle
+                            as={Button}
+                            variant="link"
+                            eventKey={`usages-popover-${command.name}`}
+                        >
+                            Usages
+                        </Accordion.Toggle>
+                        <Accordion.Collapse
+                            eventKey={`usages-popover-${command.name}`}
+                        >
+                            <ul>
+                                {command.usages.map((usage, i) => (
+                                    <li key={i}>
+                                        <code>{usage}</code>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Accordion.Collapse>
+                    </Accordion>
                 );
-                const examplesOverlay = (
-                    <Popover id={`examples-popover-${command.name}`}>
-                        <ul>
-                            {command.examples.map((example, i) => (
-                                <li key={i}>
-                                    <code>{example}</code>
-                                </li>
-                            ))}
-                        </ul>
-                    </Popover>
+                const examplesAccordion = (
+                    <Accordion>
+                        <Accordion.Toggle
+                            as={Button}
+                            variant="link"
+                            eventKey={`examples-popover-${command.name}`}
+                        >
+                            Examples
+                        </Accordion.Toggle>
+                        <Accordion.Collapse
+                            eventKey={`examples-popover-${command.name}`}
+                        >
+                            <ul>
+                                {command.examples.map((example, i) => (
+                                    <li key={i}>
+                                        <code>{example}</code>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Accordion.Collapse>
+                    </Accordion>
                 );
                 return (
                     <Card key={command.name} className="command">
@@ -43,28 +65,8 @@ export const Commands = () => (
                                 {command.tooltip}
                             </Card.Subtitle>
                             <Card.Text>{command.description}</Card.Text>
-                            <OverlayTrigger
-                                trigger="focus"
-                                placement="top"
-                                overlay={usagesOverlay}
-                            >
-                                <Card.Link>
-                                    <Button variant="outline-primary">
-                                        Usages
-                                    </Button>
-                                </Card.Link>
-                            </OverlayTrigger>
-                            <OverlayTrigger
-                                trigger="focus"
-                                placement="top"
-                                overlay={examplesOverlay}
-                            >
-                                <Card.Link>
-                                    <Button variant="outline-primary">
-                                        Examples
-                                    </Button>
-                                </Card.Link>
-                            </OverlayTrigger>
+                            {usagesAccordion}
+                            {examplesAccordion}
                         </Card.Body>
                     </Card>
                 );
